534f37dce062f7a83a36b80386c874f6368ed2a3
Lenzo <lenbunce@Lens-MacBook-Pro.local>
2016-04-07 22:02:36 -0400

    strike effect not working

---
+++ js/main.js
@@ -0,0 +1,15 @@
+var $input = $('#list-input');
+var $list = $('.list');
+
+$('form').on('submit', function (e) {
+  var $li = $('<li>');
+
+  e.preventDefault();
+  $li.html($input.val());
+  $list.append($li);
+  $input.val('');
+});
+
+$li.on('click', $li, function () {
+  $li.toggleClass('strike')
+});
